29d85458e45d74177b72d2dcf76e483f627a0f75
ch4rbo <baueratctu@gmail.com>
2021-08-29 01:38:44 +0000

    realizo cambios en app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  
+  
+  function dominio() {
+  let domain = [];
+  for (let i = 0; i < pronouns.length; i++) {
+  for (let x = 0; x < adjectives.length; x++) {
+  for (let z = 0; z < nouns.length; z++) {
+ let pronoun = pronouns[i];
+ let adjective = adjectives[x];
+ let noun = nouns[z];
+
+ domain.push(pronoun + adjective + noun);
+}
+}
+}
+return domain;
+}
+console
+
